c96c08a225063c0143e0336f5f9e6a0b755ff13b
Tenzin <chaksamt@gmail.com>
2024-12-17 01:44:10 -0600

    before oauth;

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,83 @@
-import { Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link, router } from "expo-router";
+import { useState } from "react";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSignInPress = () => {};
+
   return (
-    <SafeAreaView>
-      <Text>Sign In</Text>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView className="flex-1 bg-white">
+        <View className="flex-1 bg-white">
+          <View className="relative w-full h-[200px]">
+            <Image
+              className="z-0 w-full h-[250px] "
+              source={images.signUpCar}
+            ></Image>
+            <Text className="font-JakartaSemiBold text-2xl text-black bottom-14 left-5">
+              Welcome
+            </Text>
+          </View>
+          <View className="p-5">
+            <InputField
+              label="Email"
+              placeholder="Enter your email"
+              icon={icons.email}
+              value={form.email}
+              onChangeText={(value) => {
+                setForm({ ...form, email: value });
+              }}
+            />
+            <InputField
+              label="Password"
+              placeholder="Enter your password"
+              icon={icons.password}
+              value={form.password}
+              secureTextEntry={true}
+              onChangeText={(value) => {
+                setForm({ ...form, password: value });
+              }}
+            />
+            <CustomButton
+              title="Log In"
+              onPress={onSignInPress}
+              className="mt-6"
+            />
+
+            <OAuth />
+
+            <Link
+              href="/sign-up"
+              className="text-lg text-center text-general-200 mt-10"
+            >
+              <Text> Dont have an account? </Text>
+              <Text className="text-primary-500"> Sign up </Text>
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,6 +1,8 @@
+import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
-import { router } from "expo-router";
+import { Link, router } from "expo-router";
 import { useState } from "react";
 import {
   Image,
@@ -19,8 +21,12 @@ const SignUp = () => {
     email: "",
     password: "",
   });
+
+  const onSignUpPress = () => {};
+
   return (
     <KeyboardAvoidingView
+      style={{ flex: 1 }}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
       <ScrollView className="flex-1 bg-white">
@@ -63,6 +69,21 @@ const SignUp = () => {
                 setForm({ ...form, password: value });
               }}
             />
+            <CustomButton
+              title="Sign Up"
+              onPress={onSignUpPress}
+              className="mt-6"
+            />
+
+            <OAuth />
+
+            <Link
+              href="/sign-in"
+              className="text-lg text-center text-general-200 mt-10"
+            >
+              <Text> Already have an account? </Text>
+              <Text className="text-primary-500"> Log in </Text>
+            </Link>
           </View>
         </View>
       </ScrollView>
